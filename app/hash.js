d871aa2d2a4e363a492bd92a89699fdbd204ea10
JoaoTavares <joaon_tavares@outlook.com>
2017-05-05 16:51:25 +0100

    Add password hash

---
+++ app/hash.js
@@ -0,0 +1,24 @@
+var crypto = require('crypto');
+
+var genRandomString = function(length){
+    return crypto.randomBytes(Math.ceil(length/2))
+            .toString('hex')
+            .slice(0, length);
+};
+
+var sha512 = function(password, salt){
+    var hash = crypto.createHmac('sha512', salt);
+    hash.update(password);
+
+    var value = hash.digest('hex');
+
+    return {
+        salt: salt,
+        passwordHash: value
+    };
+};
+
+module.exports = {
+    genRandomString: genRandomString,
+    sha512: sha512
+};
--- app/routes/authenticate_routes.js
+++ app/routes/authenticate_routes.js
@@ -2,16 +2,20 @@ var fs = require('fs');
 var User = require('../models/users');
 var jwt = require('jsonwebtoken');
 var config = require('../../constants');
+var hashpwd = require('../hash');
 
 module.exports = function(router, serverlog){
 router.route('/signup')
     .post( (req, res) => {
         var user = new User();
 
+        var salt = hashpwd.genRandomString(16);
+        var passwordData = hashpwd.sha512(req.body.password, salt);
+
         user.name = req.body.name;
-        user.password = req.body.password;
+        user.password = passwordData.passwordHash;
         user.email = req.body.email;
-        //user.salt = req.body.salt;
+        user.salt = passwordData.salt;
         user.admin = true;
         user.created_on = new Date();
         user.lastUpdate = user.created_on;
@@ -38,7 +42,12 @@ router.route('/signin')
             if(!user){
                 res.status(404).json({success: false, message: 'Authentication failed! User not found.'});
             } else if(user) {
-                if(user.password != req.body.password){
+
+                var passwordData = hashpwd.sha512(req.body.password, user.salt);
+                console.log('PASSWORD AUTH:', passwordData.passwordHash);
+                console.log('USER PASSWORD:', user.password);
+                
+                if(user.password != passwordData.passwordHash){
                     res.status(404).json({success: false, message: 'Authentication failed! Wrong password.'});
                 } else {
                     var token =jwt.sign(user, config.jwtsecret, {
